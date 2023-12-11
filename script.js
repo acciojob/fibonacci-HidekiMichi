7e4ac1c82bf4ad92cd021b1f6373d77467641fb9
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2023-12-11 21:10:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
 // your code here
+	if(num==1 || num==0){
+		return 0;
+	}
+	if(num==2){
+		return 1;
+	}
+	let a=0;
+	let b=1;
+	for(let c=3;c<=num;c++){
+		let sum=a+b;
+		a=b;
+		b=sum;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
